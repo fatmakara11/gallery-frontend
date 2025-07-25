acd31fb9c281a03740cb03470a5a3bac41d76b56
fakara <farmaka646@gmail.com>
2025-07-25 05:35:29 -0700

    Backend ile Frontend API ler ile haberleşti

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,33 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ComponentHome } from './component-home/component-home';
+import { ComponentAbout } from './component-about/component-about';
+import { ComponentGallery } from './component-gallery/component-gallery';
+import { ComponentNavbar } from './component-navbar/component-navbar';
+import { ComponentDashboard } from './component-dashboard/component-dashboard';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    ComponentHome,
+    ComponentAbout,
+    ComponentGallery,
+    ComponentNavbar,
+    ComponentDashboard  // buraya ekle
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    // ComponentDashboard buradan çıkar
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentHome } from './component-home/component-home';
+import { ComponentAbout } from './component-about/component-about';
+import { ComponentGallery } from './component-gallery/component-gallery';
+import {ComponentDashboard} from './component-dashboard/component-dashboard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  // boş url gelirse /home yönlendir
+  { path: 'home', component: ComponentHome },            // /home route'u ComponentHome bileşenini gösterir
+  { path: 'about', component: ComponentAbout },
+  { path: 'gallery', component: ComponentGallery },
+  {path:'dashboard', component: ComponentDashboard },
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
