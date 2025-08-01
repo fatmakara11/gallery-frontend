985064c1ebd486c19668767b5c3e1f60e9e084d0
fakara <farmaka646@gmail.com>
2025-08-01 00:27:15 -0700

    Department ve Team kısmı bacende bağlandı

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,23 +3,57 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing-module"; // doğru dosya adı ve küçük harfli
 
-import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { ComponentHome } from './component-home/component-home';
-import { ComponentAbout } from './component-about/component-about';
-import { ComponentGallery } from './component-gallery/component-gallery';
-import { ComponentNavbar } from './component-navbar/component-navbar';
-import { ComponentDashboard } from './component-dashboard/component-dashboard';
+
+// Public Components
+import { PublicLayout } from './public/public-layout/public-layout';
+import { Home } from './public/pages/home/home';
+import { About } from './public/pages/about/about';
+import { Departments } from './public/pages/departments/departments';
+import { Teams } from './public/pages/teams/teams';
+import { Contact } from './public/pages/contact/contact';
+import { Nav } from './public/components/nav/nav';
+
+// Admin Components
+import { AdminLayout } from './admin/admin-layout/admin-layout';
+import { Login } from './admin/login/login';
+import { Dashboard } from './admin/dashboard/dashboard';
+import { DepartmentManager } from './admin/department-manager/department-manager';
+import { TeamManager } from './admin/team-manager/team-manager';
+import { EmployeeManager } from './admin/employee-manager/employee-manager';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+
+
 
 @NgModule({
   declarations: [
     App,
-    ComponentHome,
-    ComponentAbout,
-    ComponentGallery,
-    ComponentNavbar,
-    ComponentDashboard  // buraya ekle
+
+    // Public
+    PublicLayout,
+    Home,
+    About,
+    Departments,
+    Teams,
+    Contact,
+    Nav,
+
+    // Admin
+    AdminLayout,
+    Login,
+    DepartmentManager,
+    TeamManager,
+    EmployeeManager,
+    Dashboard
   ],
   imports: [
     BrowserModule,
@@ -27,7 +61,14 @@ import { ComponentDashboard } from './component-dashboard/component-dashboard';
     FormsModule,
     RouterModule,
     HttpClientModule,
-    // ComponentDashboard buradan çıkar
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
+    MatSelectModule,
+
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,19 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComponentHome } from './component-home/component-home';
-import { ComponentAbout } from './component-about/component-about';
-import { ComponentGallery } from './component-gallery/component-gallery';
-import {ComponentDashboard} from './component-dashboard/component-dashboard';
+
+// Public Components
+import { PublicLayout } from './public/public-layout/public-layout';
+import { Home } from './public/pages/home/home';
+import { About } from './public/pages/about/about';
+import { Departments } from './public/pages/departments/departments';
+import { Teams } from './public/pages/teams/teams';
+import { Contact } from './public/pages/contact/contact';
+
+// Admin Components
+import { AdminLayout } from './admin/admin-layout/admin-layout';
+import { Login } from './admin/login/login';
+import { Dashboard} from './admin/dashboard/dashboard';
+import { DepartmentManager } from './admin/department-manager/department-manager';
+import { TeamManager } from './admin/team-manager/team-manager';
+import { EmployeeManager } from './admin/employee-manager/employee-manager';
+
+// Guard (ileride eklenecek)
+//import { AuthGuard } from './core/auth.guard'; // yoksa commentle
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },  // boş url gelirse /home yönlendir
-  { path: 'home', component: ComponentHome },            // /home route'u ComponentHome bileşenini gösterir
-  { path: 'about', component: ComponentAbout },
-  { path: 'gallery', component: ComponentGallery },
-  {path:'dashboard', component: ComponentDashboard },
+  // PUBLIC ROUTES
+  {
+    path: '',
+    component: PublicLayout,
+    children: [
+      { path: '', component: Home },
+      { path: 'about', component: About},
+      { path: 'departments', component: Departments },
+      { path: 'teams', component: Teams },
+      { path: 'contact', component: Contact },
+    ]
+  },
 
+  // ADMIN LOGIN (ayrı sayfa)
+  { path: 'admin-login', component: Login },
+
+  // ADMIN ROUTES
+  {
+    path: 'admin',
+    component: AdminLayout,
+    //canActivate: [AuthGuard], // Giriş yapmamışsa yönlendirme yapılır
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      { path: 'department-manager', component: DepartmentManager },
+      { path: 'team-manager', component: TeamManager },
+      { path: 'employee-manager', component: EmployeeManager },
+    ]
+  },
+
+  // 404 redirect
+  { path: '**', redirectTo: '' }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
